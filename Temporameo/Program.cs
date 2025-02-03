1a6a4f4b05faafb5c20c29774252b47670c4fc87
AliciaGuz <gl0214032023@unab.edu.sv>
2025-02-02 20:12:02 -0600

    inicio CRUD

---
+++ Temporameo/Program.cs
@@ -0,0 +1,19 @@
+﻿using reactBackend.Models.Repository;
+
+AlumnoDAO alumnoDAO = new AlumnoDAO();
+var alumno = alumnoDAO.SelectAll();
+
+#region Select All
+foreach (var item in alumno)
+{
+    Console.WriteLine(item.Nombre);
+}
+#endregion
+
+Console.WriteLine(" ");
+#region Select By ID
+
+var SelectByID = alumnoDAO.GetById(1000);
+Console.WriteLine(SelectByID?.Nombre);
+
+#endregion
---
+++ reactBackend/Models/Repository/AlumnoDAO.cs
@@ -0,0 +1,33 @@
+﻿using reactBackend.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reactBackend.Models.Repository
+{
+    public class AlumnoDAO
+    {
+        #region Context
+        public AlumnoRegistroContext contexto = new AlumnoRegistroContext();
+        #endregion
+
+        #region Select All
+         public List<Alumno> SelectAll()
+        {
+            var alumno = contexto.Alumnos.ToList<Alumno>();
+            return alumno;
+        }
+        #endregion
+
+        #region Select by ID
+
+        public Alumno? GetById(int id) {
+           var alumno = contexto.Alumnos.Where(x => x.Id == id).FirstOrDefault();
+            return alumno == null ? null : alumno;
+        }
+        #endregion
+
+    }
+}
